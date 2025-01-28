4316228f688a1f319380df48b1a5b5bae08fc841
kd <kunjdhami26@gmail.com>
2025-01-28 16:58:11 -0700

    Add Spotlight and Motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 // import Image from "next/image";
 
+import Hero from "@/components/Hero";
+
+
 export default function Home() {
   return (
     <main className="relative
@@ -13,7 +16,7 @@ export default function Home() {
     sm:px-10 px-5
     ">
       <div className="max-w-7xl w-full">
-        <h1>Hello Kunj!</h1>
+        <Hero></Hero>
       </div>
     </main>
   );
